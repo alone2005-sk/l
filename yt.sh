bfb5c572b95ded7419f1450585b1221d674d29f3
alone2005-sk <148674552+alone2005-sk@users.noreply.github.com>
2023-12-26 22:09:30 +0530

    yt.sh

--- yt.sh
+++ yt.sh
@@ -1,1 +1,9 @@
-ffmpeg -stream_loop -1 -re -i neffex.mp4 -stream_loop -1 -re -i http://stream.zeno.fm/9kaed9hws98uv -vcodec libx264 -pix_fmt yuvj420p -maxrate 500k -preset ultrafast -r 1 -g 50 -c:a aac -b:a 128k -ar 44100 -strict experimental -video_track_timescale 1000 -b:v 150k -f flv  rtmp://a.rtmp.youtube.com/live2/{Stream-Key}
+ffmpeg -loglevel info -y -re \
+  -f image2 -loop 1 -i /home/container/oip.jpg \
+  -f concat -safe 0 -i <((for f in ./mp3/*.mp3; do path="/home/container/sk.mp3"; echo "file ${path@Q}"; done) | shuf) \
+  -c:v libx264 -preset veryfast -b:v 3000k -maxrate 3000k -bufsize 6000k \
+  -framerate 25 -video_size 1280x720 -vf "format=yuv420p" -g 50 -shortest -strict experimental \
+  -c:a aac -b:a 128k -ar 44100 \
+  -f flv rtmp://a.rtmp.youtube.com/live2/m93k-a1fe-up9z-6w9u-awfc
+
+
